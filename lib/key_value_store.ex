8de3aaf7b6d7f5920c19ea85954f63b8e7879a44
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-04-07 03:02:41 +0800

    Initialize key_valu_store app with mix

---
+++ lib/key_value_store.ex
@@ -0,0 +1,18 @@
+defmodule KeyValueStore do
+  @moduledoc """
+  Documentation for `KeyValueStore`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KeyValueStore.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
